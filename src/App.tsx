c584b60f44a280d834bcddcf5d2f6f8f870b9c94
Riceal28 <2908687620@qq.com>
2024-11-20 01:30:33 +0800

    added authenticator

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import { useAuthenticator } from '@aws-amplify/ui-react';//setp6 login function part4-for sign out
+import { AccountSettings } from '@aws-amplify/ui-react';//step10 delete user
 
 const client = generateClient<Schema>();
 
 function App() {
+
+  const { user, signOut } = useAuthenticator();//setp7 login function part5-for sign out
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+
+ //step10 delete user
+  const handleSuccess = () => {
+  alert('user has been successfully deleted')
+}//step10 delete user  
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
@@ -14,25 +23,51 @@ function App() {
   }, []);
 
   function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
+    const content = window.prompt("Todo content");
+    if (content) {
+      //const userId = user?.signInDetails?.loginId || 'anonymous';
+      client.models.Todo.create({
+        content: `${content}`,
+      });
+    }
   }
+  
+  
+  
+  function deleteTodo(id: string) { //setp1 delete function part1
+    client.models.Todo.delete({ id })
+  }
+
 
   return (
     <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
+      <h1>Welcome to the chatroom</h1>
+      <button onClick={createTodo}>Send your message</button>
       <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+      {todos.map((todo) => (
+          <li 
+            onClick={() => deleteTodo(todo.id)}
+            key={todo.id}
+            className="todo-item"
+          >
+            <div className="message-container">
+              <div className="user-info">
+              <span className="user-id-box">{user?.signInDetails?.loginId || 'anonymous'}</span>
+              <span className="timestamp right-align">{new Date(todo.createdAt).toLocaleString()}</span>
+            </div>
+            <span className="todo-content">{todo.content}</span>
+            </div>
+          </li>
         ))}
       </ul>
       <div>
-        🥳 App successfully hosted. Try creating a new todo.
+        🥳 We're glad you guys can chat together.
         <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
+
       </div>
+      <button onClick={signOut}>Sign out</button> 
+      <h1>Hello {user?.signInDetails?.loginId}</h1>
+      <AccountSettings.DeleteUser onSuccess={handleSuccess} />
     </main>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,17 @@ import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
+import { Authenticator } from '@aws-amplify/ui-react'; //setp3 login function part1
+import '@aws-amplify/ui-react/styles.css';//setp4 login function part2
 
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Authenticator signUpAttributes={[
+        'name',
+      ]}>
     <App />
+    </Authenticator>
   </React.StrictMode>
 );
